675420cc534484f000d6ae90c53d4461b3243270
VICVIJ <VICVIJ@192.168.0.123>
2020-07-22 15:30:11 -0400

    Initial commit

---
+++ JavaCrashCourse/src/com/la/javaLearning/ChildClass.java
@@ -0,0 +1,16 @@
+package com.la.javaLearning;
+
+public class ChildClass extends ParentClass {
+	
+	public ChildClass() {
+		super();
+		System.out.println("This is Child class constructor");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ChildClass chclass = new ChildClass();
+
+	}
+
+}
---
+++ JavaCrashCourse/src/com/la/javaLearning/ParentClass.java
@@ -0,0 +1,9 @@
+package com.la.javaLearning;
+
+public class ParentClass {
+
+	public ParentClass() {
+		System.out.println("This is a Parent class");
+	}
+
+}
