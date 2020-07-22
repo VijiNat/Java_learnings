6c8b3c5650ec197b0a22ad1d120effa4bfa22856
VICVIJ <VICVIJ@192.168.0.123>
2020-07-22 13:22:23 -0400

    Latest commit

--- JavaCrashCourse/src/com/la/javaLearning/ConstructorOverloading.java
+++ JavaCrashCourse/src/com/la/javaLearning/ConstructorOverloading.java
@@ -1,10 +1,22 @@
 package com.la.javaLearning;
 
 public class ConstructorOverloading {
+	
+	String shape;
+	
+	ConstructorOverloading(){
+		System.out.println("Shape object created");
+	}
+	
+	ConstructorOverloading(String shapetype){
+		shape=shapetype;
+		System.out.println("Drawing " +shapetype);
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		ConstructorOverloading shapepattern = new ConstructorOverloading();
+		ConstructorOverloading shapepattern1 = new ConstructorOverloading("Circle");
 	}
 
 }
