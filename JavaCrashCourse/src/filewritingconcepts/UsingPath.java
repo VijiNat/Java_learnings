d79c1243e8366873ff8da9235cc64a75b5291678
VICVIJ <VICVIJ@VICVIJ-PC>
2020-07-30 18:07:34 -0400

    Latest commit

--- JavaCrashCourse/src/filewritingconcepts/UsingPath.java
+++ JavaCrashCourse/src/filewritingconcepts/UsingPath.java
@@ -1,10 +1,21 @@
 package filewritingconcepts;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class UsingPath {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws IOException {
+		
+		String location = "UsingPath.txt";
+		String content = "There is no age for learning new things! Do not feel shy to try new things!";
+		
+		Path filepath = Paths.get(location);
+		Files.write(filepath,content.getBytes());
+		
+		
 	}
 
 }
